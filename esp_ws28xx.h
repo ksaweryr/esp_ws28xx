159c06123ad9b520dd420e5295ffadbe0457aacf
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2025-11-09 02:53:27 +0100

    remove global state

--- esp_ws28xx.h
+++ esp_ws28xx.h
@@ -42,9 +42,24 @@ typedef enum {
     WS2815,
 } led_strip_model_t;
 
-esp_err_t ws28xx_init(int pin, led_strip_model_t model, int num_of_leds,
-                      CRGB **led_buffer_ptr);
-void ws28xx_fill_all(CRGB color);
-esp_err_t ws28xx_update();
+#define WS28XX_CTX_OWNED_BIT 1
+#define WS28XX_DMA_BUFFER_OWNED_BIT 2
+#define WS28XX_PIXEL_BUFFER_OWNED_BIT 4
+#define WS28XX_SPI_SETTINGS_OWNED_BIT 8
+
+typedef struct {
+    uint8_t ownership_bits;
+    uint16_t *dma_buffer;
+    CRGB *ws28xx_pixels;
+    int n_of_leds, reset_delay, dma_buf_size;
+    led_strip_model_t led_model;
+    spi_settings_t* spi_settings;
+} ws28xx_t;
+
+esp_err_t ws28xx_init(ws28xx_t** ctx, int pin, led_strip_model_t model, int num_of_leds,
+                      CRGB *led_buffer, uint16_t *dma_buffer, spi_settings_t* spi_settings);
+void ws28xx_fill_all(ws28xx_t* ctx, CRGB color);
+esp_err_t ws28xx_update(ws28xx_t* ctx);
+void ws28xx_destroy(ws28xx_t* ctx);
 
 #endif /* MAIN_ESP_WS28XX_H_ */
